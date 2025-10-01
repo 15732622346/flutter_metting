cc7c31a83c0bd7b463bfa5673743e648ab9caee7
Developer <dev@example.com>
2025-10-01 14:24:44 +0800

    57-更改app版本号

--- lib/config/version_config.dart
+++ lib/config/version_config.dart
@@ -27,7 +27,7 @@ class VersionConfig {
     : '基础功能版';
     
   /// 版本号（与pubspec.yaml保持一致）
-  static String get versionNumber => IS_NEW_VERSION ? '2.0.0' : '1.5.0';
+  static String get versionNumber => IS_NEW_VERSION ? '2.0.0' : '1.8.0';
   
   // ==================== 功能开关 ====================
   
